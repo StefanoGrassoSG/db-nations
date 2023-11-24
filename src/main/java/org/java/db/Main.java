937069aff7dd29b07b31627330d843a6808048fa
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-24 15:03:41 +0100

    milestone 2 complete

---
+++ src/main/java/org/java/db/Main.java
@@ -0,0 +1,60 @@
+package org.java.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class Main {
+	
+	private static final String url = "jdbc:mysql://localhost:3306/db_nation";
+	private static final String user = "root";
+	private static final String pws = "root";
+
+	public static void main(String[] args) {
+		
+		queryTest();
+	}
+
+	private static void queryTest() {
+		
+		try (Connection con = DriverManager.getConnection(url, user, pws)) {
+			
+			final String SQL = ""
+					+ " SELECT countries.country_id, countries.name, regions.name, continents.name "
+					+ " FROM `countries` "
+					+ " JOIN regions ON countries.region_id = regions.region_id "
+					+ " JOIN continents ON regions.continent_id = continents.continent_id "
+					+ " ORDER BY countries.name ";
+			
+			try (PreparedStatement ps = con.prepareStatement(SQL)) {
+				
+				try (ResultSet rs = ps.executeQuery()) {
+					
+					int rowCounter = 0;
+					while(rs.next()) {
+						
+						final int COUNTRY_ID = rs.getInt(1);
+						final String COUNTRY_NAME = rs.getString(2);
+						final String REGION_NAME = rs.getString(3);
+						final String CONTINENT_NAME = rs.getString(4);
+						
+						System.out.println("[" + COUNTRY_ID + "] " + COUNTRY_NAME 
+								+ ": " + REGION_NAME 
+								+ "/" + CONTINENT_NAME
+								);
+						
+						rowCounter++;
+					}
+					
+					System.out.println("\n------------------------------\n");
+					System.out.println("Row count: " + rowCounter);
+				}
+			}
+		} catch (Exception e) {
+			
+			System.out.println("Error in db: " + e.getMessage());
+		}
+	}
+}
