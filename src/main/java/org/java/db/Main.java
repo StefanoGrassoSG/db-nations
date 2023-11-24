b444aa7b8fceb429c32da9c8346a494c8dbad98c
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-11-24 16:26:04 +0100

    bonus complete

--- src/main/java/org/java/db/Main.java
+++ src/main/java/org/java/db/Main.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -20,7 +22,7 @@ public class Main {
 	private static void queryTest() {
 		
 		Scanner s = new Scanner(System.in);
-		System.out.print("Parola che vuoi cercare: ");
+		System.out.print("Search: ");
 		
 		String filterWord = s.nextLine();
 		String pattern = "%" + filterWord + "%";
@@ -42,6 +44,7 @@ public class Main {
 				try (ResultSet rs = ps.executeQuery()) {
 					
 					int rowCounter = 0;
+					System.out.println("ID" + "\t   " + "COUNTRY" + "\t     " + "REGION" + "\t        " + "CONTINENT");
 					while(rs.next()) {
 						
 						final int COUNTRY_ID = rs.getInt(1);
@@ -49,21 +52,57 @@ public class Main {
 						final String REGION_NAME = rs.getString(3);
 						final String CONTINENT_NAME = rs.getString(4);
 						
-						System.out.println("[" + COUNTRY_ID + "] " + COUNTRY_NAME 
-								+ ": " + REGION_NAME 
-								+ "/" + CONTINENT_NAME
-								);
-						
+						System.out.println("[" + COUNTRY_ID + "] " + "\t   " + COUNTRY_NAME + "\t     " + REGION_NAME + "\t" + CONTINENT_NAME);
+						 
 						rowCounter++;
 					}
 					
-					System.out.println("\n------------------------------\n");
-					System.out.println("Row count: " + rowCounter);
+					System.out.println("Choose a country id: ");
+					String id = s.nextLine();
+					int idInt = Integer.valueOf(id);
+					
+					final String SQL2 = "SELECT * "
+		                    + " FROM `countries` "
+		                    + " JOIN country_languages ON countries.country_id = country_languages.country_id "
+		                    + " JOIN languages ON country_languages.language_id = languages.language_id "
+		                    + " JOIN country_stats ON countries.country_id = country_stats.country_id "
+		                    + " WHERE countries.country_id = ? "
+		                    + " ORDER BY country_stats.year, country_stats.population; ";
+
+		            try (PreparedStatement ps2 = con.prepareStatement(SQL2)) {
+		                ps2.setInt(1, idInt);
+
+		                try (ResultSet rss = ps2.executeQuery()) {
+		                	List<String> languages = new ArrayList<>();
+		                	String country_name = null;
+		                	int year = 0;
+		                	int population = 0;
+		                	String gdp = null;
+		                    while (rss.next()) {
+		                    	country_name = rss.getString(2);
+		                    	year = rss.getInt(14);
+		                    	population = rss.getInt(15);
+		                    	gdp = rss.getString(16);
+		                        languages.add(rss.getString(12));
+		                    }
+		                    
+		                    System.out.println("\nDetails for country: "
+			                        + country_name 
+			                        + "\nLanguages: " + languages
+		                    		+ "\nMost recent stats"
+				                    + "\nYear: " + year
+				                    + "\nPopulation: " + population
+		                    		+ "\nGDP: " + gdp);
+		                }
+		            } catch (Exception e) {
+		                System.out.println("Error in db: " + e.getMessage());
+		            }
 				}
 			}
 		} catch (Exception e) {
 			
 			System.out.println("Error in db: " + e.getMessage());
+			
 		}
 	}
 }
